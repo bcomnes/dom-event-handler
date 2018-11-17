2661b6510a6894124d5d9facc6d33be014748290
Bret Comnes <bcomnes@gmail.com>
2018-11-16 20:13:39 -0800

    Add tests and docs

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # dom-event-handler
+[![npm version][npmimg]][npm] [![build status][travisimg]][travis] [![coverage][coverallsimg]][coveralls]
+[![downloads][downloadsimg]][downloads] [![js-standard-style][standardimg]][standard]
+
+A generic DOM event handler.  Inspired by a @webreflection [article](https://medium.com/@WebReflection/dom-handleevent-a-cross-platform-standard-since-year-2000-5bf17287fd38).
+
+## Installation
+```console
+$ npm install dom-event-handler
+```
+
+## Usage
+
+```js
+import DOMEventHandler from "dom-event-handler"
+
+class MyWSController extends SomeOtherClass {
+  constructor () {
+    this.ws = new WebSocket('ws://localhost:8080');
+    this.handler = new DOMEventHandler(this, this.ws)
+  }
+  
+  // These methods handle the websocket events
+  onmessage (ev) {}
+  onopen (ev) {}
+  onerror (ev) {}
+  onclose (ev) {}
+}
+```
+
+## API
+
+### `handler = new DOMEventHandler(ctx, [node])`
+
+Create a new instance of DOMEventHandler passing in a context `ctx` (often `this`) and optionally an event emitter `node` (e.g. an event emitting DOM node) to attach listeners to.
+
+### `handler.addEventListeners(node)`
+
+Attach all `event` handler methods on `ctx` to the event emitter `node`.
+
+### `handler.removeEventListeners(node)`
+
+Remove all `event` handler event names on `ctx` from the event emitter `node`.
+
+### Internal Methods
+
+You don't need to call these, but they are there.
+
+#### `handler.handleEvent(event)`
+
+Implements the [`eventListener.handleEvent`](https://developer.mozilla.org/en-US/docs/Web/API/EventListener/handleEvent) method for the  `events` found on the `ctx` prototype.  This is where the magic happens.
+
+#### `handler.events`
+
+A getter that returns all events found on the `ctx` the handler is bound to.  A handler is a method on `ctx` prototype that starts with the letters `on`.
+
+
+## License
+[MIT](https://tldrlegal.com/license/mit-license)
+
+[stabilityimg]: https://img.shields.io/badge/stability-experimental-orange.svg?style=flat-square
+[stability]: https://nodejs.org/api/documentation.html#documentation_stability_index
+[npmimg]: https://img.shields.io/npm/v/jsonfeed-to-rss.svg?style=flat-square
+[npm]: https://npmjs.org/package/jsonfeed-to-rss
+[travisimg]: https://img.shields.io/travis/bcomnes/jsonfeed-to-rss/master.svg?style=flat-square
+[travis]: https://travis-ci.org/bcomnes/jsonfeed-to-rss
+[downloadsimg]: http://img.shields.io/npm/dm/jsonfeed-to-rss.svg?style=flat-square
+[downloads]: https://npmjs.org/package/jsonfeed-to-rss
+[standardimg]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat-square
+[standard]: https://github.com/feross/standard
+[coverallsimg]: https://img.shields.io/coveralls/bcomnes/jsonfeed-to-rss/master.svg?style=flat-square
+[coveralls]: https://coveralls.io/github/bcomnes/jsonfeed-to-rss
---
+++ test.js
@@ -0,0 +1,90 @@
+const test = require('tape')
+const DOMEventHandler = require('./')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+const existy = require('existy')
+
+test('can insatiate and has required API methods', t => {
+  class HandlerClass {
+    oncustomevent (ev) {
+      console.log(ev)
+    }
+  }
+  const ctx = new HandlerClass()
+  const handler = new DOMEventHandler(ctx)
+  t.equal(typeof handler.addEventListeners, 'function', 'addEventListeners is defined')
+  t.equal(typeof handler.removeEventListeners, 'function', 'removeEventListeners is defined')
+  t.equal(typeof handler.handleEvent, 'function', 'handleEvent is defined')
+  t.true(Array.isArray(handler.events), 'events getter returns an array')
+  t.equal(handler.events.length, 1, 'events getter has one event')
+  t.end()
+})
+
+test('works even without handlers', t => {
+  class HandlerClass {}
+  const ctx = new HandlerClass()
+  const handler = new DOMEventHandler(ctx)
+  t.true(Array.isArray(handler.events), 'events getter returns an array')
+  t.equal(handler.events.length, 0, 'events getter has 0 events')
+  t.end()
+})
+
+test('handles events when instantiated with a node', t => {
+  const dom = new JSDOM(`<!DOCTYPE html><p id='node'>Hello world</p>`)
+  const node = dom.window.document.querySelector('p')
+  const CustomEvent = dom.window.CustomEvent
+
+  class HandlerClass {
+    oncustomevent (ev) {
+      t.equal(ev.detail.unicorn, 'rainbows', 'handled event')
+      t.end()
+    }
+  }
+  const ctx = new HandlerClass()
+  const handler = new DOMEventHandler(ctx, node)
+  t.true(existy(handler), 'handler created')
+  var event = new CustomEvent('customevent', { detail: {unicorn: 'rainbows'} })
+
+  node.dispatchEvent(event)
+})
+
+test('handles events when attached after the fact', t => {
+  const dom = new JSDOM(`<!DOCTYPE html><p id='node'>Hello world</p>`)
+  const node = dom.window.document.querySelector('p')
+  const CustomEvent = dom.window.CustomEvent
+
+  class HandlerClass {
+    oncustomevent (ev) {
+      t.equal(ev.detail.unicorn, 'rainbows', 'handled event')
+      t.end()
+    }
+  }
+  const ctx = new HandlerClass()
+  const handler = new DOMEventHandler(ctx)
+  handler.addEventListeners(node)
+  var event = new CustomEvent('customevent', { detail: {unicorn: 'rainbows'} })
+
+  node.dispatchEvent(event)
+})
+
+test('doesnt handle events when removed', t => {
+  const dom = new JSDOM(`<!DOCTYPE html><p id='node'>Hello world</p>`)
+  const node = dom.window.document.querySelector('p')
+  const CustomEvent = dom.window.CustomEvent
+
+  class HandlerClass {
+    oncustomevent (ev) {
+      t.fail('this shouldnt run')
+    }
+  }
+  const ctx = new HandlerClass()
+  const handler = new DOMEventHandler(ctx, node)
+  t.true(existy(handler), 'handler created')
+  var event = new CustomEvent('customevent', { detail: {unicorn: 'rainbows'} })
+  handler.removeEventListeners(node)
+  node.dispatchEvent(event)
+  setTimeout(() => {
+    t.ok(true, 'event handler doesnt catch anything')
+    t.end(null)
+  }, 5)
+})
